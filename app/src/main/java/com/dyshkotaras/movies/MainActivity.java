e94f3f1d936a2ed0ae47527125b9361cfcea8f66
taras <dyshkotaras@gmail.com>
2023-04-04 22:57:12 +0300

    Lesson #87 add @Query parameter

--- app/src/main/java/com/dyshkotaras/movies/MainActivity.java
+++ app/src/main/java/com/dyshkotaras/movies/MainActivity.java
@@ -1,9 +1,13 @@
 package com.dyshkotaras.movies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.util.Log;
+
+import java.util.List;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.functions.Consumer;
@@ -13,23 +17,20 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    private MainViewModel mainViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ApiFactory.apiService.loadMovies()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<MoviesList>() {
+        mainViewModel = new ViewModelProvider(MainActivity.this).get(MainViewModel.class);
+        mainViewModel.getMoviesListLD().observe(MainActivity.this,
+                new Observer<List<Movie>>() {
                     @Override
-                    public void accept(MoviesList moviesList) throws Throwable {
-                        Log.d(TAG, moviesList.toString());
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Throwable {
-                        Log.d(TAG,throwable.toString());
+                    public void onChanged(List<Movie> movies) {
+                        Log.d(TAG,movies.toString());
                     }
                 });
+        mainViewModel.loadMovies();
     }
 }
